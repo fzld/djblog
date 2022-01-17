cbb0c6c1b2e52bce7619362246925ba8c49db422
zaldi <fzlddd@gmail.com>
2022-01-18 00:17:41 +0700

    add new and edit post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('post', views.post, name='post'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
